8a33a1c3da3939fa3bdeea48452b77bd4e85bfd2
Pawel Dziagwa <pawe0918@edu.zealand.dk>
2022-10-04 20:10:26 +0200

    a1

--- Assignment_1/FootballPlayer.cs
+++ Assignment_1/FootballPlayer.cs
@@ -1,7 +1,37 @@
-﻿namespace Assignment_1
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
 {
     public class FootballPlayer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ShirtNumber { get; set; }
+
+        public void CheckName()
+        {
+            if (Name.Length < 2)
+                throw new ArgumentException();
+        }
+
+        public void CheckAge()
+        {
+            if (Age <= 1)
+                throw new ArgumentOutOfRangeException();
+        }
+
+        public void CheckShirtNumber()
+        {
+            if (ShirtNumber < 1 || ShirtNumber > 99)
+                throw new ArgumentOutOfRangeException();
+        }
 
     }
 }
---
+++ Assignment_1Tests1/FootballPlayerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        private FootballPlayer player = new FootballPlayer { Id = 1, Name = "Abcde", Age = 25, ShirtNumber = 10 };
+        private FootballPlayer playerName = new FootballPlayer { Id = 2, Name = "A", Age = 22, ShirtNumber = 15 };
+        private FootballPlayer playerAge = new FootballPlayer { Id = 4, Name = "Abcde", Age = 0, ShirtNumber = 25 };
+        private FootballPlayer playerShirt = new FootballPlayer { Id = 5, Name = "Abcde", Age = 22, ShirtNumber = 200 };
+
+
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            player.CheckName();
+            Assert.ThrowsException<ArgumentException>(() => playerName.CheckName());
+        }
+
+        [TestMethod()]
+        public void ValidateAgeTest()
+        {
+            player.CheckAge();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => playerAge.CheckAge());
+        }
+
+        [TestMethod()]
+        public void ValidateShirtNumberTest()
+        {
+            player.CheckShirtNumber();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => playerShirt.CheckShirtNumber());
+        }
+
+    }
+}
